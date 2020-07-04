f90f00d7f204d2d173d6c459fb99e74313777a34
Snorlee <brianlee132@gmail.com>
2020-07-04 03:33:30 -0400

    first commit

---
+++ routes/index.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var router = express.Router();
+var moment = require('moment');
+const messages = [
+  {
+    text: 'Sup Michigan Gamer',
+    user: 'Amanda',
+    added: moment(new Date()).format('MMM Do 2018, 7:22:36 a'),
+  },
+  {
+    text: 'boomer',
+    user: 'Charles',
+    added: moment(new Date()).format('MMM Do 2019, 10:22:44 a'),
+  },
+];
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Leave Yo Message', messages });
+});
+
+//Post Form//
+router.post('/', (req, res) => {
+  const newMessage = {
+    text: req.body.message,
+    user: req.body.user,
+    added: moment(new Date()).format('MMMM Do YYYY, h:mm:ss a'),
+  };
+  messages.push(newMessage);
+  res.redirect('/');
+});
+
+module.exports = router;
